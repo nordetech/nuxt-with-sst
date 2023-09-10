2faf235a532fe36b7b61bd0757706f065008c0ab
Arpad Gabor <hello@arpadgabor.com>
2023-09-10 22:32:32 +0300

    init

---
+++ sst.config.ts
@@ -0,0 +1,14 @@
+import { SSTConfig } from "sst";
+import { Web } from "./stacks/web";
+
+export default {
+  config(_input) {
+    return {
+      name: "sst-nuxt",
+      region: "eu-west-1",
+    };
+  },
+  stacks(app) {
+    app.stack(Web);
+  },
+} satisfies SSTConfig;
